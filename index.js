c83062ab7d02f3a2128d946dc7da3c65150faba9
Ajaykartheek Vasagiri <AVasagiri@walmart.com>
2016-01-06 23:10:53 -0800

    updated with chokidar

--- index.js
+++ index.js
@@ -8,24 +8,24 @@ let mime = require('mime-types')
 let rimraf = require('rimraf')
 let mkdirp = require('mkdirp')
 let yargs = require('yargs')
+let chokidar = require('chokidar')
 
 require('songbird')
 // require('longjohn')
 
 let nssocket = require('nssocket');
-
-
 let argv = require('yargs')
-    .argv
+  .default('dir', process.cwd())
+  .argv
 
 //let bluebird = require('bluebird')
-
 //bluebird.longStackTraces()
-
 
 const NODE_ENV = process.env.NODE_ENV
 const PORT = process.env.PORT || 8000
-const ROOT_DIR = argv.dir || path.resolve(process.cwd())
+const ROOT_DIR = path.resolve(path.join(argv.dir, 'client'))
+
+const TCP_PORT = 8001
 
 let app = express()
 
@@ -37,23 +37,31 @@ if(NODE_ENV == 'development')
 app.listen(PORT, ()=> console.log('LISTENING at http://127.0.0.1:{PORT}'))
 
 
-var server = nssocket.createServer(function (socket) {
-    // 
-    // Here `socket` will be an instance of `nssocket.NsSocket`. 
-    // 
-    socket.send(['you', 'there']);
-    socket.data(['iam', 'here'], function (data) {
-      // 
-      // Good! The socket speaks our language  
-      // (i.e. simple 'you::there', 'iam::here' protocol) 
-      // 
-      // { iam: true, indeedHere: true } 
-      // 
-      console.dir(data);
-    })
-  });
+var sockets = [];
+var server = nssocket.createServer(
+    function (socket) {
+        console.log('inside socket')
+        sockets.push(socket);
+        socket.data('Connecting', function (data) {
+            console.log("There are now", sockets.length);
 
-server.listen(6785);
+            for(var i=0, l=sockets.length; i<l; i++) {
+                sockets[i].send('Broadcasting', 'data');
+            }
+        });
+    }
+    ).listen(TCP_PORT);
+console.log(`LISTENING ${TCP_PORT} for Dropbox Clients`)
+
+let watcher = chokidar.watch('.', {ignored: /[\/\\]\./,ignoreInitial: true})
+
+
+// Add event listeners. 
+watcher
+  .on('add', path => broadCastData('write', false, path))
+  .on('change', path => broadCastData('write', false, path))
+  .on('unlink', path => broadCastData('delete', false, path));
+
 
 app.get('*', setFileMetaData, sendHeaders, (req,res) =>
 {
@@ -93,13 +101,10 @@ app.put('*', setFileMetaData, setDirDetails, (req, res, next) =>
 			return;
 		}
 		await mkdirp.promise(req.dirPath)
-
-		if(!req.isDirectory)
-		{
+		if(!req.isDirectory){
 			req.pipe(fs.createWriteStream(req.filepath))
 			res.end()
 		}
-
 	}().catch(next)	
 })
 
@@ -111,27 +116,31 @@ app.post('*', setFileMetaData, setDirDetails, (req, res, next) =>
 			res.send(405,'File does not exists')
 			return;
 		}
-
 		if(req.isDirectory) {
 			res.send(405,'Path is a Directory')
 			return;
 		}
-
 		await fs.promise.truncate(req.filepath,0)
-
 		req.pipe(fs.createWriteStream(req.filepath))
 		res.end()
-		
-
 	}().catch(next)	
 })
 
+function broadCastData(action, dir, path)
+{
+	let jsonString = '{ "action": "' + action + '","path":'
+	jsonString = jsonString +  '"' + path + '","type": "'
+	jsonString = jsonString + dir + '"}'
+
+	for(var i=0, l=sockets.length; i<l; i++) {
+        sockets[i].send('Broadcasting', jsonString);
+
+    }
+}
+
 function setDirDetails(req, res, next)
 {
-	
-
 		let filepath = req.filepath
-
 		let endWithSlash = filepath.charAt(filepath.length-1) === path.sep
 		let hasExt = path.extname(filepath) != ''
 		req.isDirectory = endWithSlash || !hasExt
@@ -143,41 +152,30 @@ function setFileMetaData(req, res, next)
 {
 
 		let filepath = path.resolve(path.join(ROOT_DIR,req.url))
-		if(filepath.indexOf(ROOT_DIR) != 0)
-		{
+		if(filepath.indexOf(ROOT_DIR) != 0)		{
 			res.send(400,'Invalid path provided')
 			return;
 		}
-
-		req.filepath = filepath
-		
+		req.filepath = filepath		
 		fs.promise.stat(req.filepath)
 		 	.then(stat => req.stat = stat, () => req.stat = null)
 		 	.nodeify(next)
-		
-
 }
 
 function sendHeaders(req, res, next)
 {
 	nodeify(async ()=> {
-
-        if(!req.stat)
-		{
+        if(!req.stat){
 			res.send(400,'Invalid file')
 			return;
 		}
-
-        if(req.stat.isDirectory())
-        {
+        if(req.stat.isDirectory()){
         	let files = await fs.promise.readdir(req.filepath)
         	res.body = JSON.stringify(files)
         	res.setHeader('Content-Length', res.body.length)
-        	res.setHeader('Content-Type', 'application/text')
-        	
+        	res.setHeader('Content-Type', 'application/text')        	
         	return
         }
-
         res.setHeader('Content-Length', req.stat.size)
         let contentType = mime.contentType(path.extname(req.filepath))
         res.setHeader('Content-Type', contentType)
